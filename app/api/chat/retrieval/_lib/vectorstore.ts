0c669ed12972a2edebaa07381bd8d976db53a203
Achmad Ilmi Al Akbar <dznilmi@gmail.com>
2025-04-25 13:34:51 +0700

    fix: Fix Undetectable Missing ENV Value Error For Neon Connection String

--- app/api/chat/retrieval/_lib/vectorstore.ts
+++ app/api/chat/retrieval/_lib/vectorstore.ts
@@ -4,6 +4,6 @@ import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
 export async function loadVectorStore() {
   const embeddings = new GoogleGenerativeAIEmbeddings();
   return await NeonPostgres.initialize(embeddings, {
-    connectionString: process.env.NEON_CONNECTION_STRING ?? "",
+    connectionString: process.env.NEON_CONNECTION_STRING as string,
   });
 }
